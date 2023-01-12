8f633d72fc9956c0071d8bab58ff7cbc75da08af
Tim <tim.nyul@gmaio.com>
2023-01-12 20:02:15 +0100

    added image capture test

---
+++ src/camera_turtlebot/test/test_camera.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import patch
+import rclpy
+import sys
+import os
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+from src.camera_turtlebot.camera_turtlebot.camera_node import Camera
+
+
+
+class MyTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        with patch("cv2.VideoCapture", return_value=cv2.VideoCapture(os.path.dirname(__file__) + "/testimg.jpg")):
+            self.test_node = Camera()
+        self.input_img = cv2.imread(os.path.dirname(__file__) + "/testimg.jpg")
+
+    def tearDown(self):
+        self.test_node.destroy_node()
+
+    def test_captureImage(self):
+        ret, frame = self.test_node.vid.read()
+        self.assertEqual(ret, True)
+        self.assertEqual(frame.all(), self.input_img.all())
+
+
+if __name__ == '__main__':
+    unittest.main()
