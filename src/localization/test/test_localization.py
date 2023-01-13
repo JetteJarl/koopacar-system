31cd34c74c42849bb1c8ca839b2ef28553771775
holio00 <63851239+holio00@users.noreply.github.com>
2023-01-13 23:03:43 +0100

    added tests for npqueue in localization node

---
+++ src/localization/test/test_localization.py
@@ -0,0 +1,52 @@
+import unittest
+import rclpy
+import sys
+import os
+import numpy as np
+
+sys.path.append(os.path.dirname(__file__) + "/../localization")
+from localization_node import NpQueue
+
+
+class NpQueueTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.MAX_Q_LEN = 5
+        self.ELEM_DIM = 2
+        self.tested_queue = NpQueue(self.MAX_Q_LEN, self.ELEM_DIM)
+
+    def tearDown(self):
+        pass
+
+    def test_addElement(self):
+        # create input
+        input_elem = [1, 1]
+        # add input to queue
+        self.tested_queue.push(input_elem)
+        # test queue
+        self.assertEqual(self.tested_queue.q[0].tolist(), input_elem)
+
+    def test_addOverflowElement(self):
+        # create input/output
+        input_elem = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]]
+        overflow_elem = [5, 5]
+        output_elem = [[5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]
+        # add input to queue
+        [self.tested_queue.push(elem) for elem in input_elem]
+        self.tested_queue.push(overflow_elem)
+        # test queue
+        self.assertEqual(len(self.tested_queue.q), self.MAX_Q_LEN)
+        self.assertEqual(self.tested_queue.q.tolist(), output_elem)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
