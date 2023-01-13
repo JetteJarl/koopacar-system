e1531097fb6f3baa88991931e85c8af5e851ecfe
JetteJarl <knopp.henriette@gmail.com>
2023-01-13 20:37:23 +0100

    Updated .prepare-env.sh

--- .prepare-env.sh
+++ .prepare-env.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 source ~/.bashrc
 
-_CUR_PATH=$(pwd)
-
+# Install missing dependencies
 rosdep install -i --from-path src --rosdistro foxy -y
+
+# Build packages unless an option is specified
+if [ "$1" != "--no-build" ]; then
+    colcon build --packages-select perception localization
+fi
+
+# source package and nodes
+. install/setup.bash
+
 colcon build
-cd $_CUR_PATH
 
 source install/local_setup.bash
 
