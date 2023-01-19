875953b85b2fcee91e567c26e9be1fd81a1d673e
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-01-19 13:05:34 +0100

    Cleaned up imports in test files

--- src/camera_turtlebot/test/test_camera.py
+++ src/camera_turtlebot/test/test_camera.py
@@ -2,13 +2,11 @@ import unittest
 from unittest.mock import patch
 import rclpy
 import os
-import sys
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from src.camera_turtlebot.camera_turtlebot.camera_node import Camera
 
-sys.path.append(os.path.dirname(__file__) + "/../camera_turtlebot")
-from camera_node import Camera
 
 class CameraNodeCaptureImageTest(unittest.TestCase):
 
@@ -36,6 +34,7 @@ class CameraNodeCaptureImageTest(unittest.TestCase):
         self.assertEqual(ret, True)
         self.assertEqual(frame.all(), self.input_img.all())
 
+
 class CameraNodePublishImageTest(unittest.TestCase):
 
     @classmethod
@@ -53,7 +52,8 @@ class CameraNodePublishImageTest(unittest.TestCase):
         # create decoy node to subscribe to topic
         self.sub_node = rclpy.create_node("sub_camera")
         self.captured_imgmsg = []
-        self.sub_node.create_subscription(Image, '/camera_turtlebot/image_raw', lambda img: self.captured_imgmsg.append(img), 10)
+        self.sub_node.create_subscription(Image, '/camera_turtlebot/image_raw',
+                                          lambda img: self.captured_imgmsg.append(img), 10)
         self.input_img = cv2.imread(os.path.dirname(__file__) + "/testimg.jpg")
         self.bridge = CvBridge()
 
@@ -69,6 +69,7 @@ class CameraNodePublishImageTest(unittest.TestCase):
         self.assertEqual(len(self.captured_imgmsg), 1)
         captured_img = self.bridge.imgmsg_to_cv2(self.captured_imgmsg[0])
         self.assertEqual(captured_img.all(), self.input_img.all())
+
 
 if __name__ == '__main__':
     unittest.main()
