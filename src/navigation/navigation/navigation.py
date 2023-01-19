7a8ceb77f49f36389798feef0e05f74e3024114b
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-01-19 10:03:07 +0100

    reformatted navigation.py

--- src/navigation/navigation/navigation.py
+++ src/navigation/navigation/navigation.py
@@ -4,6 +4,7 @@ from std_msgs.msg import Float32MultiArray, MultiArrayDimension
 from geometry_msgs.msg import Twist
 import numpy as np
 
+
 class Navigation(Node):
     def __init__(self):
         super().__init__("navigation")
@@ -13,16 +14,15 @@ class Navigation(Node):
 
         self.publish_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
 
-        self.cones = None # cones published by map
-        self.old_pos = np.array([0, -1]) # old roboter position
+        self.cones = None  # cones published by map
+        self.old_pos = np.array([0, -1])  # old roboter position
         self.bot_pos = np.array([0, 0])  # current roporter position
 
-        self.target = np.array([0, 1]) # position of target
+        self.target = np.array([0, 1])  # position of target
 
         self.BLUE = 0
         self.ORANGE = 1
         self.YELLOW = 2
-
 
     def received_cones(self, msg):
         self.cones = np.array(msg.data).reshape((-1, msg.layout.dim[1].size))
@@ -30,17 +30,17 @@ class Navigation(Node):
         # get cones in front of bot
         v_movement = bot_pos - old_pos
         is_in_front = np.dot(cones[1:], v_movement) > 0
-        
+
         cones_in_front = cones[is_in_front]
 
         # abort if no cones in front
         if cones_in_front.size == 0:
-                target = None
-                return
+            target = None
+            return
 
         # get closest yellow and blue
-        cones_blue = cones_in_front[cones_in_front[:,0] == self.BLUE]
-        cones_yellow =  cones_in_front[cones_in_front[:,0] == self.YELLOW]
+        cones_blue = cones_in_front[cones_in_front[:, 0] == self.BLUE]
+        cones_yellow = cones_in_front[cones_in_front[:, 0] == self.YELLOW]
 
         closest_blue_ind = np.argmin(np.linalg.norm(cones_blue[1:] - bot_pos, 2))
         closest_blue = cones_blue[closest_blue_ind]
@@ -50,8 +50,6 @@ class Navigation(Node):
 
         # calculate target
         self.target = (closest_blue[1:] + closet_yellow[1:]) / 2
-
-
 
     def update_pos(self, msg):
         old_pos = bot_pos
@@ -68,18 +66,13 @@ class Navigation(Node):
 
             publish_cmd_vel.publish(twist_msg)
 
-
-            
         trajectory = target - bot_pos
         angle = np.arctan2(trajectory[0], trajectory[1]) - np.arctan2(bot_pos[0], bot_pos[1])
 
-
         twist_msg.linear[2] = 0.1
-        twist_msg.angular[2] = angle/10
+        twist_msg.angular[2] = angle / 10
 
         publish_cmd_vel.publish(twist_msg)
-
-
 
 
 def main(args=None):
