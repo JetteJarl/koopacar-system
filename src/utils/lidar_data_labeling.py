1507d06269ed82ee19c842d1290f4d34945aaf70
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-01-30 13:32:49 +0100

    Added clustering to create initial labels

---
+++ src/utils/lidar_data_labeling.py
@@ -0,0 +1,68 @@
+import os
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+from src.utils.plot_data import plot_labled_data_3d
+
+
+def lidar_data_labeling(data):
+    """ Use clustering approach to find groups of points."""
+
+    VAR_UPPER_LIMIT = 1000
+    VAR_LOWER_LIMIT = 0.0000001
+    CONE_LABEL = 1
+    OTHER_LABEL = 0
+
+    EPSILON_RANGE = 0.15
+    MIN_SAMPLES = 5
+
+    cluster_labels = DBSCAN(eps=EPSILON_RANGE, min_samples=MIN_SAMPLES).fit_predict(data)
+
+    labels = np.zeros(cluster_labels.shape, dtype=np.uint8)
+
+    for indices, label in enumerate(np.unique(cluster_labels)):
+        cluster = data[cluster_labels == label]
+        cluster_var = np.var(cluster, axis=0)
+        cluster_indices = np.argwhere(cluster_labels == label)
+
+        if VAR_UPPER_LIMIT > np.sum(cluster_var[:2]) > VAR_LOWER_LIMIT and label != -1:
+            labels[cluster_indices] = CONE_LABEL
+        else:
+            labels[indices] = OTHER_LABEL
+
+    return labels
+
+
+def main(args=None):
+    PATH_TO_DATA = "/home/ubuntu/koopacar-system/data/raw_lidar_set"
+    PATH_TO_LABEL = "/home/ubuntu/koopacar-system/data/00/label"
+
+    for file in sorted(os.listdir(PATH_TO_DATA)):
+        try:
+            data_input = open(os.path.join(PATH_TO_DATA, file), "r")
+            data = []
+            for line in data_input:
+                if line == "\n":
+                    continue
+
+                point = list(map(float, line[1:len(line) - 2].split(",")))
+                data.append(point)
+
+            labels = lidar_data_labeling(np.array(data))
+
+            label_file = open(os.path.join(PATH_TO_LABEL, os.path.splitext(file)[0] + ".label"), 'w')
+            for label in labels:
+                label_file.write(str(label))
+                label_file.write("\n")
+
+            label_file.close()
+            data_input.close()
+
+            plot_labled_data_3d(data, labels, title=file)
+        except OSError:
+            print("Could not open/read file: " + args)
+
+
+if __name__ == '__main__':
+    main()
