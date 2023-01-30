8912c1b21f30e841bba5e8a271345feb23dc3a9a
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-01-30 13:32:06 +0100

    Added utility functions for plotting data

---
+++ src/utils/plot_data.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_raw_points_3d(data):
+    data = np.array(data)
+
+    plt.scatter(data[::, 0], data[::, 1], s=0.5)
+
+    plt.show()
+
+
+def plot_labled_data_3d(data, labels, cone_label=1, title=''):
+    data = np.array(data)
+
+    color = ['red' if label == cone_label else 'grey' for label in labels]
+
+    plt.scatter(data[:, 0], data[:, 1], c=color, s=0.5)
+
+    plt.title(title)
+    plt.show()
+
+
+def main(args=None):
+    try:
+        data_input = open("../../data/raw_lidar_set/" + args, "r")
+        data = []
+        for line in data_input:
+            if line == "\n":
+                continue
+
+            point = list(map(float, line[1:len(line) - 2].split(",")))
+            data.append(point)
+
+        plot_raw_points_3d(data)
+    except OSError:
+        print("Could not open/read file: " + args)
+
+
+if __name__ == '__main__':
+    main("lidar_scan20230130-112604.txt")
