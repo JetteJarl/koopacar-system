402e2c42c889c00907fbf5cbd252830c5871a946
holio00 <63851239+holio00@users.noreply.github.com>
2023-02-05 23:41:25 +0100

    modified ploting

--- src/utils/plot_data.py
+++ src/utils/plot_data.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from src.utils.point_transformation import convert_FLU_to_ENU
+from src.utils.point_transformation import convert_ENU_to_FLU
+from src.utils.point_transformation import translation
+
 
 def plot_raw_points_3d(data):
     data = np.array(data)
@@ -25,19 +29,60 @@ def plot_labled_data_3d(data, labels, cone_label=1, title='', xlim=(-4, 4), ylim
 
 def main(args=None):
     try:
-        data_input = open("../../data/new_data_set/" + args, "r")
-        data = []
-        for line in data_input:
+        # lidar scan
+        scan_input = open("../../data/lidar_perception/new_data_set/lidar_scan/" + args + ".txt", "r")
+        scan = []
+        scan_and_cones = []
+
+        for line in scan_input:
             if line == "\n":
                 continue
 
             point = list(map(float, line[1:len(line) - 2].split(",")))
-            data.append(point)
+            scan.append(point)
+            scan_and_cones.append(point)
 
-        plot_raw_points_3d(data)
+        # corresponding labels
+        label_input = open("../../data/lidar_perception/new_data_set/label/" + args + ".label", "r")
+        labels = []
+        for line in label_input:
+            if line == "\n":
+                continue
+
+            label = int(line[0])
+            labels.append(label)
+
+        # cones
+        cones_input = open("../../data/lidar_perception/new_data_set/cone_pos.txt", "r")
+        cones = []
+        for line in cones_input:
+            if line == "\n":
+                continue
+
+            cone = list(map(float, line[1:len(line) - 2].split(",")))
+            cones.append(cone)
+
+        cones = convert_FLU_to_ENU(cones)
+        for cone in cones:
+            scan_and_cones.append(cone)
+
+        #TODO: cleanup
+        #plot_raw_points_3d(scan_and_cones)
+        plot_labled_data_3d(scan, labels)
+        if False:
+            plt.xlim((-4, 4))
+            plt.ylim((-4, 4))
+            scan = np.array(scan)
+            cones = np.array(cones)
+            cones = translation(cones, convert_ENU_to_FLU([[-0.143209, -1.16353, 0]]))
+            plt.scatter(scan[:, 0], scan[:, 1], s=0.5)
+            plt.scatter(cones[:, 0], cones[:, 1], color="red", s=1)
+
+            plt.title("")
+            plt.show()
     except OSError:
         print("Could not open/read file: " + args)
 
 
 if __name__ == '__main__':
-    main("lidar_scan20230130-112604.txt")
+    main("lidar_scan_20230202-120538")
