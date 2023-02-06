237b55920557728e45395924237caf68ce53cc7d
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-02-06 13:59:43 +0100

    Using plotting is work in progress

--- src/utils/plot_data.py
+++ src/utils/plot_data.py
@@ -28,7 +28,6 @@ def plot_labled_data_3d(data, labels, cone_label=1, title='', xlim=(-4, 4), ylim
 
 
 def main(args=None):
-    try:
         # lidar scan
         scan_input = open("../../data/lidar_perception/new_data_set/lidar_scan/" + args + ".txt", "r")
         scan = []
@@ -52,6 +51,8 @@ def main(args=None):
             label = int(line[0])
             labels.append(label)
 
+
+
         # cones
         cones_input = open("../../data/lidar_perception/new_data_set/cone_pos.txt", "r")
         cones = []
@@ -59,30 +60,31 @@ def main(args=None):
             if line == "\n":
                 continue
 
+            temp1 = line[1:len(line) - 2].split(",")
+            temp = map(float, line[1:len(line) - 2].split(","))
             cone = list(map(float, line[1:len(line) - 2].split(",")))
             cones.append(cone)
 
-        cones = convert_FLU_to_ENU(cones)
+        #cones = convert_FLU_to_ENU(cones)
         for cone in cones:
             scan_and_cones.append(cone)
 
         #TODO: cleanup
         #plot_raw_points_3d(scan_and_cones)
         plot_labled_data_3d(scan, labels)
-        if False:
+        if True:
             plt.xlim((-4, 4))
             plt.ylim((-4, 4))
             scan = np.array(scan)
             cones = np.array(cones)
-            cones = translation(cones, convert_ENU_to_FLU([[-0.143209, -1.16353, 0]]))
+            #cones = translation(cones, convert_ENU_to_FLU([[-0, -0, 0]]))
             plt.scatter(scan[:, 0], scan[:, 1], s=0.5)
             plt.scatter(cones[:, 0], cones[:, 1], color="red", s=1)
 
             plt.title("")
             plt.show()
-    except OSError:
-        print("Could not open/read file: " + args)
+
 
 
 if __name__ == '__main__':
-    main("lidar_scan_20230202-120538")
+    main("lidar_scan_20230206-132801")
