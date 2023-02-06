bugfix in rotation function/test

src/utils/point_transformation.py
@@ -37,7 +37,7 @@ def rotation(points, rotation_angle, rotation_origin=(0, 0)):
     o = np.atleast_2d(rotation_origin)
     np_points = np.atleast_2d(np_points)
 
-    return np.squeeze((R @ (np_points.T-o.T) + o.T).T)
+    return (R @ (np_points.T-o.T) + o.T).T
 
 
 def radians_from_quaternion(x, y, z, w):