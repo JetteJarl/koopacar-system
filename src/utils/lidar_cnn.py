5de30c805aae964140076c3cbe2ffcecc26160b9
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-02-14 10:04:13 +0100

    Added point cloud to matrix method and tests.

--- src/utils/lidar_cnn.py
+++ src/utils/lidar_cnn.py
@@ -1,94 +1,45 @@
 import numpy as np
 
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
 
-# This code was stolen from
-# https://pytorch.org/tutorials/beginner/nn_tutorial.html#refactor-using-dataloader
-# And hopfully works
-# But it prob needs a few hours of fixing xD ups
-# mein Gehirn ist iwann auf standby gegangen und hat nur noch copy-paste xD
+def lidar_matrix(points, resolution=(256, 256), lidar_range=3.5):
+    """
+    Creates image/matrix matching the top-down view of point cloud described by points.
 
-class LidarCnn():
-    def __init__(self):
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(16, 2, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.AdaptiveMaxPool2d(1),
-            Lambda(lambda x: x.view(x.size(0), -1)),
-        )
+    points --> points gathered from Lidar scan, representation of point cloud in FLU
+    optional:
+    resolution --> resolution of the target matrix/image
+    lidar_range --> max range of the lidar being used
+    """
+    # TODO: Find more efficient implementation
+    points = np.array(points)
+    point_matrix = np.zeros(resolution)
 
-    @staticmethod
-    def loss_batch(model, loss_func, xb, yb, opt=None):
-        loss = loss_func(model(xb), yb)
+    step_size_x = lidar_range * 2 / resolution[0]
+    step_size_y = lidar_range * 2 / resolution[1]
 
-        if opt is not None:
-            loss.backward()
-            opt.step()
-            opt.zero_grad()
+    y_upper = lidar_range
+    y_lower = y_upper - step_size_y
 
-        return loss.item(), len(xb)
+    for row_ind in range(0, resolution[0]):
+        x_upper = lidar_range
+        x_lower = x_upper - step_size_x
 
-    @staticmethod
-    def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
-        for epoch in range(epochs):
-            model.train()
-            for xb, yb in train_dl:
-                LidarCnn.loss_batch(model, loss_func, xb, yb, opt)
+        for column_ind in range(0, resolution[1]):
+            is_in_x_range = np.logical_and(points[:, 0] < x_upper, points[:, 0] >= x_lower)
+            is_in_y_range = np.logical_and(points[:, 1] < y_upper, points[:, 1] >= y_lower)
 
-            model.eval()
-            with torch.no_grad():
-                losses, nums = zip(
-                    *[LidarCnn.loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl]
-                )
-            val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
+            for is_x, is_y in zip(is_in_x_range, is_in_y_range):
+                if is_x and is_y:
+                    point_matrix[row_ind, column_ind] = 1
 
-            print(epoch, val_loss)
+            x_upper -= step_size_x
+            x_lower -= step_size_x
 
-    @staticmethod
-    def get_data(train_ds, valid_ds, bs):
-        return (
-            DataLoader(train_ds, batch_size=bs, shuffle=True),
-            DataLoader(valid_ds, batch_size=bs * 2)
-        )
+        y_upper -= step_size_y
+        y_lower -= step_size_y
 
-    @staticmethod
-    def preprocess(x, y):
-        return x.view(-1, 1, 28, 28), y
-
-class WrappedDataLoader:
-    def __init__(self, dl, func):
-        self.dl = dl
-        self.func = func
-
-    def __len__(self):
-        return len(self.dl)
-
-    def __iter__(self):
-        batches = iter(self.dl)
-        for b in batches:
-            yield (self.func(*b))
-
-class Lambda(nn.Module):
-    def __init__(self, func):
-        super().__init__()
-        self.func = func
-
-    def forward(self, x):
-        return self.func(x)
+    return point_matrix
 
 
-def main(args=None):
-    train_dl, valid_dl = LidarCnn.get_data(train_ds, valid_ds, bs= 64)
-    train_dl = WrappedDataLoader(train_dl, LidarCnn.preprocess)
-    valid_dl = WrappedDataLoader(valid_dl, LidarCnn.preprocess)
-
-    model = LidarCnn()
-    opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-
-    LidarCnn.fit(epochs=1, model=model, loss_func=, opt=opt, train_dl=, valid_dl=)
+def create_input(filename):
+    pass
