93e963880e875616fb48eb04173f5d8ad9e7b4d3
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-02-15 15:37:07 +0100

    Implemented test.

--- src/utils/test/test_lidar_cnn.py
+++ src/utils/test/test_lidar_cnn.py
@@ -1,9 +1,9 @@
 import unittest
 import numpy as np
-from src.utils.lidar_cnn import lidar_matrix
 import matplotlib.pyplot as plt
 
 from src.utils.file_operations import *
+from src.utils.lidar_cnn import lidar_matrix
 
 
 class LidarCNNTest(unittest.TestCase):
@@ -17,18 +17,19 @@ class LidarCNNTest(unittest.TestCase):
 
     def setUp(self):
         self.test_file_path = "files/test_scan.bin"
+        self.test_results_path = "files/test_result_256.txt"
 
     def tearDown(self):
         pass
 
-    @unitest.skip("Not yet implemented. Assertion is missing.")
+    # @unittest.skip("Not yet implemented. Assertion is missing.")
     def test_lidar_matrix(self):
-        # TODO: Define expected test result and add assertion accordingly
         test_input = np.array(list_from_file(self.test_file_path))
 
         result = lidar_matrix(test_input)
+        expected = list_from_file(self.test_results_path)
 
-
+        np.testing.assert_allclose(result, expected)
 
 
 if __name__ == '__main__':
