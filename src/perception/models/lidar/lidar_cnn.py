cf26f4769b18ca5139b9409a396a0f4e4a2ba168
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-03-01 10:28:25 +0100

    Skip .gitkeep, changed model layers.

--- src/perception/models/lidar/lidar_cnn.py
+++ src/perception/models/lidar/lidar_cnn.py
@@ -10,6 +10,7 @@ from src.utils.plot_data import plot_lidar_cnn_results
 class LidarCNN:
     def __init__(self, scan_length=360):
         pass
+
     def __gather_input(self):
         pass
 
@@ -21,15 +22,18 @@ class LidarCNN:
 
 
 def main():
-    data_dir = "/data/lidar_perception/training_data"
+    data_dir = "/home/ubuntu/koopacar-system/data/lidar_perception/training_data/lidar_01"
     scans_dir = os.path.join(data_dir, "ranges")
-    label_dir = os.path.join(data_dir, "labels")
+    label_dir = os.path.join(data_dir, "label")
 
     all_scans = sorted(os.listdir(scans_dir))
     all_labels = sorted(os.listdir(label_dir))
 
     ranges = []  # shape --> N x 360
     for scan_file in all_scans:
+        if scan_file == ".gitkeep":
+            continue
+
         with open(os.path.join(scans_dir, scan_file)) as file:
             range_string = file.read().replace('\n', ' ')
             # beam_range = np.array([np.array(entry) for entry in np.fromstring(range_string, dtype=float, sep=' ')])
@@ -38,6 +42,9 @@ def main():
 
     labels = []
     for label_file in all_labels:
+        if label_file == ".gitkeep":
+            continue
+
         with open(os.path.join(label_dir, label_file)) as file:
             label_string = file.read().replace('\n', ' ')
             label = np.fromstring(label_string, dtype=int, sep=' ')
@@ -52,21 +59,22 @@ def main():
 
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, input_shape=(360, 1), padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=5, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=5, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=5, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=16, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=1, kernel_size=3, padding='same', activation='relu'))
+    # TODO: Add dense (?)
 
     model.summary()
 
     model.compile(
         optimizer='adam',
-        loss=tf.keras.losses.BinaryCrossentropy(),
+        loss=tf.keras.losses.MeanSquaredError(),
         metrics=[tf.keras.metrics.BinaryAccuracy(),
                  tf.keras.metrics.Recall(),
                  tf.keras.metrics.Precision()]
