5af7f2d2669b3ab928aad62b6c31fa84121140a2
holio00 <63851239+holio00@users.noreply.github.com>
2023-03-03 02:01:26 +0100

    WIP sensor fusion node + test

--- src/localization/localization/sensor_fusion_node.py
+++ src/localization/localization/sensor_fusion_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 from src.utils.flex_queue import FlexibleQueue
+import numpy as np
 
 
 class SensorFusionNode(Node):
@@ -12,26 +13,47 @@ class SensorFusionNode(Node):
 
         # create subscriber
         self.subscriber_bboxes_ = self.create_subscription(Float32MultiArray, '/bounding_boxes', self.received_bbox, 10)
-        self.subscriber_cone_points = self.create_subscription(Float32MultiArray, '/all_objects', self.received_cone_points, 10)
+        self.subscriber_cone_points = self.create_subscription(Float32MultiArray, '/all_objects',
+                                                               self.received_cone_points, 10)
         # create publisher
         self.publisher_cones = self.create_publisher(Float32MultiArray, '/cone_position', 10)
 
         # queues for storing messages
-        self.bounding_boxes_buffer = FlexibleQueue(10)
-        self.cone_points_buffer = FlexibleQueue(10)
+        self.bounding_boxes_buffer = FlexibleQueue(30)
+        self.cone_points_buffer = FlexibleQueue(30)
 
-    def received_bbox(self, bboxes):
-        self.bounding_boxes_buffer.push(bboxes)
-        # TODO: Check for matching timestamps on elements in buffers
+        # consts
+        self.FOV = 62
 
-    def received_cone_points(self, points):
-        self.cone_points_buffer.push(points)
-        # TODO: Check for matching timestamps on elements in buffers
+    def received_bbox(self, msg):
+        """Safes bboxes to queue and runs sensor fusion"""
+        self.bounding_boxes_buffer.push(msg)
+        if len(self.bounding_boxes_buffer) > 0 and len(self.cone_points_buffer) > 5:
+            self.fusion(msg)
 
-    def fusion(self, bboxes, cone_points):
+    def received_cone_points(self, msg):
+        self.cone_points_buffer.push(msg)
+
+    def fusion(self, bbox_msg):
+        # save bounding boxes
+        bboxes = np.array(bbox_msg.data)
+        bboxes = bboxes.reshape((-1, bbox_msg.layout.dim[1].size))[1:]
+
+        # save time stamp of bounding boxes
+        bboxes_stamp_sec = int(bboxes[0, 0])
+        bboxes_stamp_nano = int(bboxes[0, 1])
+        bboxes_stamp = float(f"{bboxes_stamp_sec}.{bboxes_stamp_nano}")
+
+        # get synchronized scan points
+        scans = []  # TODO (how is /all_objects msg formatted?)
+
+        # get fov of buffer
+        buffer_fov = [np.concatenate((item[int(-self.FOV / 2):], item[:int(self.FOV / 2)])) for item in scans]
+
+        return bboxes  # TODO: remove return (only for testing during development)
         cones = Float32MultiArray()
-        # TODO: Implement sensor fusion
         self.publisher_cones.publish(cones)
+        return cones
 
 
 def main(args=None):
--- src/localization/test/test_sensor_fusion.py
+++ src/localization/test/test_sensor_fusion.py
@@ -1,8 +1,11 @@
 import unittest
 import rclpy
+from std_msgs.msg import Float32MultiArray, MultiArrayDimension
+import numpy as np
+from src.localization.localization.sensor_fusion_node import SensorFusionNode
 
 
-class SensorFusionTest(unittest.TestCase):
+class SensorFusionNodeTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         rclpy.init()
@@ -12,11 +15,29 @@ class SensorFusionTest(unittest.TestCase):
         rclpy.shutdown()
 
     def setUp(self):
-        pass
+        self.test_sensor_fusion_node = SensorFusionNode()
 
     def tearDown(self):
-        pass
+        self.test_sensor_fusion_node.destroy_node()
 
+    def test_fusion(self):
+        test_bbox_a = [1., 1., 50., 50., 95., 1.]
+        test_bbox_b = [100., 1., 150., 50., 95., 1.]
+
+        test_msg = Float32MultiArray()
+        test_msg.layout.dim.append(MultiArrayDimension())
+        test_msg.layout.dim.append(MultiArrayDimension())
+        test_msg.layout.dim[0].label = 'Bounding Boxes'
+        test_msg.layout.dim[0].size = 2  # 2 bounding boxes
+        test_msg.layout.dim[1].label = 'Coordinates'
+        test_msg.layout.dim[1].size = 6  # (top-left-x, top-left-y, bottom-right-x, bottom-right-y, confidence, class)
+        test_msg.data = test_bbox_a + test_bbox_b
+
+        expected_result = np.array([1., 1., 50., 50., 95., 1., 100., 1., 150., 50., 95., 1.])
+
+        result = self.test_sensor_fusion_node.fusion(test_msg)
+
+        np.testing.assert_allclose(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
