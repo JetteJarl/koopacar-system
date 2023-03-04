53be494638b142941a7d26eb13072fd68e68942e
holio00 <63851239+holio00@users.noreply.github.com>
2023-03-04 21:11:34 +0100

    function converting bbox messages to data + test

--- src/utils/message_operations.py
+++ src/utils/message_operations.py
@@ -1,3 +1,34 @@
+import numpy as np
+
+
 def header_to_float_stamp(header):
     """Converts the stamp of header to float."""
     return float(f"{header.stamp.sec}.{header.stamp.nanosec}")
+
+
+def bbox_msg_to_values(bbox_msg):
+    """
+    Returns the data and time stamp of a bounding box message.
+
+    Returns:
+        bboxes: np.array of bounding boxes in format (top-left-x,
+                top-left-y, bottom-right-x, bottom-right-y,
+                confidence, class)
+        bboxes_stamp: float of timestamp in format seconds.nanoseconds
+    """
+    # save data of message
+    data = np.array(bbox_msg.data)
+    data = data.reshape((-1, bbox_msg.layout.dim[1].size))
+
+    # extract bounding boxes
+    if data.size > 6:
+        bboxes = data[1:]
+    else:
+        bboxes = np.array([])
+
+    # extract time stamp of bounding boxes
+    bboxes_stamp_sec = int(data[0, 0])
+    bboxes_stamp_nano = int(data[0, 1])
+    bboxes_stamp = float(f"{bboxes_stamp_sec}.{bboxes_stamp_nano}")
+
+    return bboxes, bboxes_stamp
