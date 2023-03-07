0aa185e27791a1f58aec72acf04faa3f182e778d
holio00 <63851239+holio00@users.noreply.github.com>
2023-03-07 12:19:00 +0100

    _detect_cones() in sensor_fusion_node.py

--- src/localization/localization/sensor_fusion_node.py
+++ src/localization/localization/sensor_fusion_node.py
@@ -5,6 +5,7 @@ from src.utils.flex_queue import FlexibleQueue
 from src.utils.message_operations import bbox_msg_to_values, centroid_msg_to_values
 #from koopacar_interfaces import Centroids
 import numpy as np
+import math
 
 
 class SensorFusionNode(Node):
@@ -26,6 +27,8 @@ class SensorFusionNode(Node):
 
         # consts
         self.CAMERA_FOV = 62
+        self.IMG_WIDTH = 640
+        self.IMG_HEIGHT = 480
         self.STAMP_DIFF_THRESHOLD = 0.1  # TODO: create as parameter
         self.NUM_COMPARED_SCANS = 5
 
@@ -101,9 +104,49 @@ class SensorFusionNode(Node):
         #scan_stamps = [self.scan_buffer.get[i] for i in range(len(self.cone_points_buffer))]
         pass
 
-    def _detect_cones(self, bbox_data, centroids_data):
-        """Returns detected cones from the bounding box and centroids data"""
-        pass
+    def _detect_cones(self, bboxes, centroids):
+        """
+        Returns detected cones from the bounding box and centroids data
+
+        The returned cones is in format [x, y, label].
+        """
+        if len(centroids) == 0:
+            return np.array([])
+
+        # calculate angle of each centroid
+        centroid_angles = [math.atan2(-centroid[1], centroid[0]) for centroid in centroids]
+        centroid_angles = [math.degrees(centroid_angle) + self.CAMERA_FOV/2 for centroid_angle in centroid_angles]
+
+        # sort centroids by distance to the bot/[0, 0]
+        sorted_centroid_indices = np.argsort(np.linalg.norm(centroids, ord=2, axis=1))
+
+        # bool map whether a centroid is assigned to a bounding box
+        used_centroids = np.zeros((len(sorted_centroid_indices)))
+
+        # fov to pixels ratio (width)
+        fov_px_ratio = self.CAMERA_FOV / self.IMG_WIDTH
+
+        # save labeled centroids
+        labeled_centroids = []
+
+        # iterate over bounding boxes, starting with the biggest/highest
+        for bb in sorted(bboxes, key=lambda bb: bb[3] - bb[1], reverse=True):
+            # approx angles of bounding box start/end
+            start_angle = bb[0] * fov_px_ratio
+            end_angle = bb[2] * fov_px_ratio
+
+            # check for matching, not used centroids
+            for idx in sorted_centroid_indices:
+                if start_angle - 1 <= centroid_angles[idx] <= end_angle + 1 and not used_centroids[idx]:
+                    labeled_centroids.append((bb[5], centroids[idx]))
+                    used_centroids[idx] = 1
+                    break
+
+        detected_cones = np.empty((len(labeled_centroids), 3))
+        for i, (label, centroid) in enumerate(labeled_centroids):
+            detected_cones[i] = centroid[0], centroid[1], label
+
+        return detected_cones
 
 
 def main(args=None):
