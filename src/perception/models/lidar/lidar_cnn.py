dc39ebf89dfb4d33727723ed614da128baf954c1
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-03-10 12:29:10 +0100

    Changed model to output probability for each class

--- src/perception/models/lidar/lidar_cnn.py
+++ src/perception/models/lidar/lidar_cnn.py
@@ -1,7 +1,45 @@
 import tensorflow as tf
+import numpy as np
+
+
+def labels_to_probability(y, num_classes=3):
+    """
+    Takes a vector of labels and creates for every label a vector with probability for each label.
+
+    y           --> labels; shape = (N, 360, ) or (N, 360, 1)
+    num_classes --> number of classes that are possible
+    returns: vector with probability for each label, the entry with index matching the label  should be 1 the rest 0.
+             shape = (N, 360, num_classes)
+    """
+    Y = np.zeros((y.shape[0], y.shape[1], num_classes))
+
+    for scan_index, scan_labels in enumerate(y):
+        for label_index, label in enumerate(scan_labels):
+            Y[scan_index, label_index, label] = 1
+
+    return Y
+
+
+def probability_to_labels(y):
+    """
+    Takes the output from the lidar_cnn which is a vector of probabilities the length of the number of possible classes.
+
+    y --> vectors with probabilities; shape = (N, 360, num_classes)
+    return: vectors with labels; shape = (N, 360)
+    """
+
+    Y = np.zeros((y.shape[0], y.shape[1]), dtype=int)
+
+    for pred_index, prediction in enumerate(y):
+        for vect_index, prob_vector in enumerate(prediction):
+            label = np.where(prob_vector == np.max(prob_vector))[0]
+            Y[pred_index, vect_index] = label
+
+    return Y
 
 
 def create_model():
+    """ Creates a CNN with 1d convolutions that can be used to detect cones in lidar scans. """
     model = tf.keras.Sequential()
 
     model.add(
@@ -26,6 +64,6 @@ def create_model():
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
     model.add(tf.keras.layers.Conv1D(filters=16, kernel_size=3, padding='same', activation='relu'))
-    model.add(tf.keras.layers.Conv1D(filters=1, kernel_size=3, padding='same', activation='relu'))
+    model.add(tf.keras.layers.Conv1D(filters=3, kernel_size=3, padding='same', activation='softmax'))
 
     return model
--- src/perception/models/lidar/train.py
+++ src/perception/models/lidar/train.py
@@ -1,57 +1,70 @@
 import os
 import numpy as np
-import sklearn
+import sklearn.model_selection
 import tensorflow as tf
 from tensorflow import keras
 
-from src.perception.models.lidar.lidar_cnn import create_model
+from src.perception.models.lidar.lidar_cnn import *
 
-data_dir = "/home/ubuntu/koopacar-system/data/lidar_perception/training_data/lidar_03"
-scans_dir = os.path.join(data_dir, "ranges")
-label_dir = os.path.join(data_dir, "label")
 
-all_scans = sorted(os.listdir(scans_dir))
-all_labels = sorted(os.listdir(label_dir))
+def train():
+    data_dir = "/home/ubuntu/koopacar-system/data/lidar_perception/training_data/lidar_03"
+    scans_dir = os.path.join(data_dir, "ranges")
+    label_dir = os.path.join(data_dir, "label")
 
-ranges = []  # shape --> N x 360
-for scan_file in all_scans:
-    if scan_file == ".gitkeep":
-        continue
+    all_scans = sorted(os.listdir(scans_dir))
+    all_labels = sorted(os.listdir(label_dir))
 
-    with open(os.path.join(scans_dir, scan_file)) as file:
-        range_string = file.read().replace('\n', ' ')
-        # beam_range = np.array([np.array(entry) for entry in np.fromstring(range_string, dtype=float, sep=' ')])
-        beam_range = np.fromstring(range_string, dtype=float, sep=' ')
-        ranges.append(beam_range)
+    ranges = []  # shape --> N x 360
+    for scan_file in all_scans:
+        if scan_file == ".gitkeep":
+            continue
 
-labels = []
-for label_file in all_labels:
-    if label_file == ".gitkeep":
-        continue
+        with open(os.path.join(scans_dir, scan_file)) as file:
+            range_string = file.read().replace('\n', ' ')
+            # beam_range = np.array([np.array(entry) for entry in np.fromstring(range_string, dtype=float, sep=' ')])
+            beam_range = np.fromstring(range_string, dtype=float, sep=' ')
+            ranges.append(beam_range)
 
-    with open(os.path.join(label_dir, label_file)) as file:
-        label_string = file.read().replace('\n', ' ')
-        label = np.fromstring(label_string, dtype=int, sep=' ')
-        labels.append(np.array(label))
+    labels = []
+    for label_file in all_labels:
+        if label_file == ".gitkeep":
+            continue
 
-X = np.array(ranges)
-Y = np.array(labels)
-X = np.expand_dims(X, axis=2)  # Model Input --> shape = (N, 360, 1) ???
-# Y = np.expand_dims(Y, axis=2)  # Model Output --> shape = (N, 360, 1) ???
+        with open(os.path.join(label_dir, label_file)) as file:
+            label_string = file.read().replace('\n', ' ')
+            label = np.fromstring(label_string, dtype=int, sep=' ')
+            labels.append(np.array(label))
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, shuffle=False)
+    X = np.array(ranges)
+    Y = np.array(labels)
 
-model = create_model()
+    X = np.expand_dims(X, axis=2)  # Model Input --> shape = (N, 360, 1) ???
+    if Y.shape != (X.shape[0], X.shape[1], 3):
+        Y = labels_to_probability(Y)
+    # Y = np.expand_dims(Y, axis=2)  # Model Output --> shape = (N, 360, 1) ???
 
-model.summary()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2, shuffle=False)
 
-model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.MeanSquaredError(),
-    metrics=[tf.keras.metrics.Recall(),
-             tf.keras.metrics.Precision()]
-)
+    model = create_model()
 
-history = model.fit(x_train, y_train, batch_size=16, epochs=64, validation_data=(x_test, y_test))
+    model.summary()
 
-model.save_weights("./weights/")
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.MeanSquaredError(),
+        metrics=[tf.keras.metrics.Recall(),
+                 tf.keras.metrics.Precision()]
+    )
+
+    history = model.fit(x_train, y_train, batch_size=16, epochs=64, validation_data=(x_test, y_test))
+
+    model.save_weights("./weights/")
+
+
+def main():
+    train()
+
+
+if __name__ == '__main__':
+    main()
