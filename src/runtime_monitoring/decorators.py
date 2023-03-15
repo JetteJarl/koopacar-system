b8d67b813165e90a333745293821ba3b9c8c6d01
holio00 <63851239+holio00@users.noreply.github.com>
2023-03-15 21:10:18 +0100

    added decorator to log execution time of functions

--- src/runtime_monitoring/decorators.py
+++ src/runtime_monitoring/decorators.py
@@ -15,7 +15,7 @@ def log_output(function):
     def wrapper(*args, **kwargs):
         output = function(*args)
 
-        output_path = os.path.join("/home/ubuntu/active-workspace/koopacar-system/monitoring/functions/", function.__name__)
+        output_path = os.path.join("/home/ubuntu/active-workspace/koopacar-system/monitoring/functions/", function.__name__, "output")
         os.makedirs(output_path, exist_ok=True)
 
         time_stamp_seconds = time.strftime("%Y%m%d-%H%M%S")
@@ -30,6 +30,41 @@ def log_output(function):
 
             time_stamp_nanoseconds = str(round(time.time_ns() * 1000)) + ": "
             file.write(time_stamp_nanoseconds + string_output + "\n")
+
+        return output
+    return wrapper
+
+
+def log_time(function):
+    """
+    Decorator to saves the computation time of a function to a file.
+
+    Creates one file for each second.
+    All times in that second will be written to that file.
+    Each output starts with a timestamp in nanoseconds.
+    Warning: If used in combination with other decorators,
+             this must be the most inner decorator.
+             Example: @any_other_decorator
+                      @log_time
+                      def any_function():
+                        ...
+    """
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        start_time = time.time_ns()
+        output = function(*args)
+        end_time = time.time_ns()
+        run_time = end_time - start_time
+
+        output_path = os.path.join("/home/ubuntu/active-workspace/koopacar-system/monitoring/functions/", function.__name__, "time")
+        os.makedirs(output_path, exist_ok=True)
+
+        time_stamp_seconds = time.strftime("%Y%m%d-%H%M%S")
+        file_path = os.path.join(output_path, function.__name__ + time_stamp_seconds + ".log")
+
+        with open(file_path, 'a') as file:
+            time_stamp_nanoseconds = str(round(time.time_ns() * 1000)) + ": "
+            file.write(time_stamp_nanoseconds + str(run_time) + "\n")
 
         return output
     return wrapper
