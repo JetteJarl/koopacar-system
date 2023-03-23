81e54308aad57e81845c91b0f19ba4a6cccbcab9
holio00 <63851239+holio00@users.noreply.github.com>
2023-03-23 17:53:08 +0100

    added string as possible message type

--- src/runtime_monitoring/runtime_monitoring/decorators_publish_node.py
+++ src/runtime_monitoring/runtime_monitoring/decorators_publish_node.py
@@ -1,6 +1,6 @@
 import numpy as np
 from rclpy.node import Node
-from std_msgs.msg import Int32, Float32, Float32MultiArray, MultiArrayDimension
+from std_msgs.msg import String, Int32, Float32, Float32MultiArray, MultiArrayDimension
 
 
 class DecoratorPublishNode(Node):
@@ -11,36 +11,39 @@ class DecoratorPublishNode(Node):
     type to the given topic.
     """
 
-    def __init__(self, function_output, topic_name):
+    def __init__(self, data, topic_name):
         super().__init__('decorator_publish_node')
 
         # create message
         msg = None
-        if isinstance(function_output, int):
+        if isinstance(data, int):
             msg = Int32()
-            msg.data = function_output
-        elif isinstance(function_output, float):
+            msg.data = data
+        elif isinstance(data, float):
             msg = Float32()
-            msg.data = function_output
-        elif isinstance(function_output, list):
+            msg.data = data
+        elif isinstance(data, str):
+            msg = String()
+            msg.data = data
+        elif isinstance(data, list):
             msg = Float32MultiArray()
-            arr = np.array(function_output)
+            arr = np.array(data)
             for i in range(arr.ndim):
                 msg.layout.dim.append(MultiArrayDimension())
             for i, shape in enumerate(arr.shape):
                 msg.layout.dim[i].size = shape
             arr = [float(value) for value in arr.flatten().tolist()]
             msg.data = arr
-        elif isinstance(function_output, np.ndarray):
+        elif isinstance(data, np.ndarray):
             msg = Float32MultiArray()
-            for i in range(function_output.ndim):
+            for i in range(data.ndim):
                 msg.layout.dim.append(MultiArrayDimension())
-            for i, shape in enumerate(function_output.shape):
+            for i, shape in enumerate(data.shape):
                 msg.layout.dim[i].size = shape
-            arr = [float(value) for value in function_output.flatten().tolist()]
+            arr = [float(value) for value in data.flatten().tolist()]
             msg.data = arr
         else:
-            raise ValueError(f"{type(function_output)} not added to supported message types")
+            raise ValueError(f"{type(data)} not added to supported message types")
 
         # create publisher
         self.publisher = self.create_publisher(type(msg), topic_name, 10)
