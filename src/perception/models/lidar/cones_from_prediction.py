07c5dcdc1f458c21f69f4d5e8a3b419224cb5d8b
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-04-02 17:57:52 +0200

    return bounding boxes with cones

--- src/perception/models/lidar/cones_from_prediction.py
+++ src/perception/models/lidar/cones_from_prediction.py
@@ -7,8 +7,21 @@ CONE_RADIUS = 0.2
 
 
 def get_cone_centroids(labels, points, ranges):
+    """
+        Takes lidar ranges, points and labels form lidar cnn and outputs cone centroids
+
+        labels --> shape: (360, 3)
+        points --> shape: (360, 2)
+        ranges --> shape: (360, 1)
+
+        returns: cones --> list of length N with ([x, y], array([x, y], shape(l, 2)) - [[cones, [points in cones]]]
+    """
     cone_points = points[labels == CONE_LABEL]
     cone_ranges = ranges.reshape(-1, )[labels == CONE_LABEL]
+
+    if len(cone_points) == 0:
+        return []
+
     cluster_labels_all = DBSCAN(eps=0.1, min_samples=3).fit_predict(cone_points)
 
     cones = []
