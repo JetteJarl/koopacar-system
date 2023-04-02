2b87a8969d5fd745fe35bcbd5cca5713406789c2
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-04-02 17:58:01 +0200

    bugfix

--- src/localization/fusion.py
+++ src/localization/fusion.py
@@ -7,16 +7,16 @@ IMG_WIDTH = 640
 IMG_HEIGHT = 480
 
 
-def _detect_cones(bboxes, centroids):
+def detect_cones(bboxes, centroids):
     """
     Returns detected cones from the bounding box and centroids data
 
     This method is what performs the sensor fusion.
 
-    The returned cones is in format [x, y, label].
+    The returned cones is in format [x, y, label] and the used bounidng boxes
     """
     if len(centroids) == 0 or len(bboxes) == 0:
-        return np.array([])
+        return np.array([]), np.array([])
 
     # calculate angle of each centroid
     centroid_angles = [math.atan2(-centroid[1], centroid[0]) for centroid in centroids]
@@ -33,6 +33,7 @@ def _detect_cones(bboxes, centroids):
 
     # save labeled centroids
     labeled_centroids = []
+    used_bboxes = []
 
     # iterate over bounding boxes, starting with the biggest/highest
     for bb in sorted(bboxes, key=lambda bb: bb[3] - bb[1], reverse=True):
@@ -44,6 +45,7 @@ def _detect_cones(bboxes, centroids):
         for idx in sorted_centroid_indices:
             if start_angle - 1 <= centroid_angles[idx] <= end_angle + 1 and not used_centroids[idx]:
                 labeled_centroids.append((bb[5], centroids[idx]))
+                used_bboxes.append(bb)
                 used_centroids[idx] = 1
                 break
 
@@ -51,4 +53,4 @@ def _detect_cones(bboxes, centroids):
     for i, (label, centroid) in enumerate(labeled_centroids):
         detected_cones[i] = centroid[0], centroid[1], label
 
-    return detected_cones
+    return detected_cones, used_bboxes
